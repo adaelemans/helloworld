66d1f100435e170339d85b8dc464157f3f5b17a3
Lucas Ledesma <l.ledesma.byd@b-yond.com>
2018-02-06 12:35:15 -0300

    adding HelloWorld tests

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,9 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloWorldTest {
+
+    @org.junit.jupiter.api.Test
+    void one() {
+        assertEquals(1,(new HelloWorld().one()));
+    }
+}
